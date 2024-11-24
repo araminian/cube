091f836dd1acadeb446a5f38052b3559d2fceee6
araminian <rmin.aminian@gmail.com>
2024-11-24 11:32:33 +0100

    Add manager skeleton

---
+++ manager/manager.go
@@ -0,0 +1,22 @@
+package manager
+
+import (
+	"github.com/araminian/cube/task"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Manager struct {
+	Pending       queue.Queue
+	TaskDb        map[string][]*task.Task
+	EventDb       map[string][]*task.TaskEvent
+	Workers       []string
+	WorkerTaskMap map[string][]uuid.UUID
+	TaskWorkerMap map[uuid.UUID]string
+}
+
+func (m *Manager) SelectWorker() {}
+
+func (m *Manager) UpdateTasks() {}
+
+func (m *Manager) SendWork() {}
